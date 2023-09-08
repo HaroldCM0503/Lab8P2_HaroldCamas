b2183604e3779b3c1aa77e9910a4f02798ab16a7
Harold Camas <haroldcamas99@gmail.com>
2023-09-08 14:46:03 -0600

    Añadir Eventos

--- src/lab8p2_haroldcamas/Main.java
+++ src/lab8p2_haroldcamas/Main.java
@@ -42,6 +42,13 @@ public class Main extends javax.swing.JFrame {
         sp_medallasPais = new javax.swing.JSpinner();
         bt_añadirPais = new javax.swing.JButton();
         jp_añadirEvento = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        cb_estiloEvento = new javax.swing.JComboBox<>();
+        cb_distanciaEvento = new javax.swing.JComboBox<>();
+        sp_recordEvento = new javax.swing.JSpinner();
+        bt_añadirEvento = new javax.swing.JButton();
         jp_añadirNadador = new javax.swing.JPanel();
         jp_listar = new javax.swing.JPanel();
         jp_simulacion = new javax.swing.JPanel();
@@ -51,6 +58,8 @@ public class Main extends javax.swing.JFrame {
         jLabel1.setText("Nombre");
 
         jLabel2.setText("Medallas");
+
+        sp_medallasPais.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
 
         bt_añadirPais.setText("Añadir Pais");
         bt_añadirPais.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -97,15 +106,65 @@ public class Main extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Agregar Pais", jp_añadirPais);
 
+        jLabel3.setText("Estilo");
+
+        jLabel4.setText("Distancia");
+
+        jLabel5.setText("Tiempo Record (s)");
+
+        cb_estiloEvento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Libre", "Pecho", "Dorso", "Mariposa" }));
+
+        cb_distanciaEvento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "100m", "200m", "400m", "800m" }));
+
+        sp_recordEvento.setModel(new javax.swing.SpinnerNumberModel(0.0d, 0.0d, null, 0.5d));
+
+        bt_añadirEvento.setText("Añadir Evento");
+        bt_añadirEvento.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_añadirEventoMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jp_añadirEventoLayout = new javax.swing.GroupLayout(jp_añadirEvento);
         jp_añadirEvento.setLayout(jp_añadirEventoLayout);
         jp_añadirEventoLayout.setHorizontalGroup(
             jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 801, Short.MAX_VALUE)
+            .addGroup(jp_añadirEventoLayout.createSequentialGroup()
+                .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jp_añadirEventoLayout.createSequentialGroup()
+                        .addGap(129, 129, 129)
+                        .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5))
+                        .addGap(103, 103, 103)
+                        .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(cb_distanciaEvento, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cb_estiloEvento, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(sp_recordEvento)))
+                    .addGroup(jp_añadirEventoLayout.createSequentialGroup()
+                        .addGap(181, 181, 181)
+                        .addComponent(bt_añadirEvento, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(390, Short.MAX_VALUE))
         );
         jp_añadirEventoLayout.setVerticalGroup(
             jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 469, Short.MAX_VALUE)
+            .addGroup(jp_añadirEventoLayout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(cb_estiloEvento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addComponent(cb_distanciaEvento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addGroup(jp_añadirEventoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5)
+                    .addComponent(sp_recordEvento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(56, 56, 56)
+                .addComponent(bt_añadirEvento, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(158, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Agregar Evento", jp_añadirEvento);
@@ -153,7 +212,7 @@ public class Main extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1)
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 801, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -171,6 +230,7 @@ public class Main extends javax.swing.JFrame {
             Pais p = new Pais(tf_nombrePais.getText(), 
                     (int) sp_medallasPais.getModel().getValue());
             paises.add(p);
+            
             File file = new File("./PaisesParticipantes");
             FileOutputStream fo = null;
             ObjectOutputStream os = null;
@@ -195,6 +255,41 @@ public class Main extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Pais añadido exitosamente!");
         }
     }//GEN-LAST:event_bt_añadirPaisMouseClicked
+
+    private void bt_añadirEventoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_añadirEventoMouseClicked
+        if(cb_distanciaEvento.getSelectedIndex() < 0 || cb_estiloEvento.getSelectedIndex() < 0 || (double) sp_recordEvento.getModel().getValue() == 0){
+            JOptionPane.showMessageDialog(this, "Tiene que llenar las casillas!");
+        }
+        else{
+            Evento e = new Evento((String) cb_estiloEvento.getSelectedItem(), 
+                    (String) cb_estiloEvento.getSelectedItem(), 
+                    (double) sp_recordEvento.getModel().getValue());
+            eventos.add(e);
+            
+            File file = new File("./Eventos");
+            FileOutputStream fo = null;
+            ObjectOutputStream os = null;
+            try {
+                fo = new FileOutputStream(file,true);
+                os = new ObjectOutputStream(fo);
+                
+                os.writeObject(e);
+                os.flush();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+            try {
+                fo.close();
+                os.close();
+            } catch (Exception e2) {
+            }
+            
+            cb_distanciaEvento.setSelectedIndex(-1);
+            cb_estiloEvento.setSelectedIndex(-1);
+            sp_recordEvento.getModel().setValue(0);
+            JOptionPane.showMessageDialog(this, "Evento añadido exitosamente!");
+        }
+    }//GEN-LAST:event_bt_añadirEventoMouseClicked
 
     /**
      * @param args the command line arguments
@@ -235,9 +330,15 @@ public class Main extends javax.swing.JFrame {
     ArrayList<Nadador> nadadores = new ArrayList();
     ArrayList<Evento> eventos = new ArrayList();
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bt_añadirEvento;
     private javax.swing.JButton bt_añadirPais;
+    private javax.swing.JComboBox<String> cb_distanciaEvento;
+    private javax.swing.JComboBox<String> cb_estiloEvento;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JPanel jp_añadirEvento;
     private javax.swing.JPanel jp_añadirNadador;
@@ -245,6 +346,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JPanel jp_listar;
     private javax.swing.JPanel jp_simulacion;
     private javax.swing.JSpinner sp_medallasPais;
+    private javax.swing.JSpinner sp_recordEvento;
     private javax.swing.JTextField tf_nombrePais;
     // End of variables declaration//GEN-END:variables
 }
